d288ca8a0111ec3159979cf960db057d6eb56dfe
christotchalakov <christo3535@hotmail.com>
2023-01-16 18:57:14 +0100

    ajout du controlleur put

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -4,9 +4,9 @@
 const Sauce = require('../models/sauce')
 
 
-
 /*********************************************************************************/
 /*************************** Routage de la ressource Sauce */
+
 exports.getAllSauces = (req,res)  => {
   Sauce.find()
     .then((sauces) => res.status(200).json({data: sauces}))
@@ -22,28 +22,44 @@ exports.getSauce = (req,res)  => {
 exports.addSauce = (req,res) =>{
   const sauceObject = JSON.parse(req.body.sauce)
   delete sauceObject._id
-  delete sauceObject.userId       //voir s'il ne faut pas ecrire ._userId
-  const lasauce = new Sauce ({
+  delete sauceObject._userId       //voir s'il ne faut pas ecrire ._userId
+  const laSauce = new Sauce ({
       ...sauceObject,
       userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   })
-  lasauce.save()
+  laSauce.save()
     .then(() => res.status(201).json({message: "Sauce crée !"}))
-    .catch(err => res.status(500).json({message: 'Le serveur ne marche pas', erro: err}))
+    .catch(err => res.status(500).json({message: 'Le serveur ne marche pas', error: err}))
+}
+
+exports.updateSauce = (req,res) => {
+  const laSauce = req.file ? {
+     ...JSON.parse(req.body.sauce),
+     image: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body};
+
+  delete laSauce._userId;
+  Sauce.findOne({_id: req.params.id})
+    .then((sauce) => {
+      if(sauce.userId != req.auth.userId){
+        res.status(401).json({message: 'Vous ne pouvez pas modifier cette sauce'})
+      }else{
+        Sauce.updateOne({_id: req.params.id},{ ...laSauce, _id: req.params.id})
+        .then(() => res.status(200).json({message: 'Sauce update'}))
+        .catch(err => res.status(401).json({message: 'ERROR',error: err}))
+      }
+    })
+    .catch(err => res.status(500).json({message:'Error', error: err}))
+
 
 
 }
 
-exports.updateSauce = () => {
-
-
-}
-
-exports.deleteSauce = ()  => {
+exports.deleteSauce = (req,res)  => {
 
 }
 
-exports.likeSauces  = ()  => {
+exports.likeSauces  = (req,res)  => {
 
 }
--- back/server.js
+++ back/server.js
@@ -3,10 +3,8 @@
 
 const express = require("express");
 const cors = require("cors");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-/*****************************/
-/******** Import de la conection à la DB */
 
 
 /************************************************/
@@ -24,15 +22,18 @@ app.use(express.urlencoded({ extended: true }));
 const user_router = require("./routes/users");
 
 const sauce_router = require("./routes/sauces");
+const path = require("path");
 
 /*******************************************************************/
 /************************ Mis en place du routage *****************/
 
 app.get("/", (req, res) => res.send("Je suis encore online!!!!!"));
 
- app.use("/api/auth", user_router);
+app.use("/api/auth", user_router);
 
- app.use("/api/sauces", sauce_router);
+app.use("/api/sauces", sauce_router);
+
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.all("*", (req, res) => res.status(501).send("Mauvaise recherche"));
 
@@ -45,14 +46,12 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app
-      .listen(process.env.SERVER_PORT, () => {
-        console.log(
-          `Le serveur marche sur le port ${process.env.SERVER_PORT || "3000"}`
-        );
-        console.log("Connexion à MongoDB réussie !");
-      })
-    })
+    app.listen(process.env.SERVER_PORT, () => {
+      console.log(
+        `Le serveur marche sur le port ${process.env.SERVER_PORT || "3000"}`
+      );
+      console.log("Connexion à MongoDB réussie !");
+    });
+  })
 
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
